14f3db44bffa661a5c25bbce6481d3b0692a5f3f
adrien-dimitri <amaugueret@gmail.com>
2021-11-22 19:53:27 +0100

    added = functionality, allowing successive operations

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let number = "";
+let storedNumber = "";
+let firstNum = "";
+let secondNum = "";
 function calculator() {
     const display = document.querySelector(".display p");
         document.addEventListener("click", e => {
@@ -9,36 +11,56 @@ function calculator() {
             if (button in [1,2,3,4,5,6,7,8,9,10]) {
                 const symbol = document.createTextNode(button);
                 display.appendChild(symbol);
-                number += button;
-                console.log(number)
+                storedNumber += button;
             };
+
             if (button === "C") {
-                while(display.firstChild) {
-                    display.removeChild(display.lastChild);
-                
-                number = "";
-                console.log(number);
-                }
+                clearDisplay(display);
+                storedNumber = "";
+                firstNum = "";
+                secondNum = "";
             };
+
             if (button === "+" || button === "-" || button === "/" || button === "*") {
                 switch (button) {
                     case "+":
                         operand = "add"
+                        break;
                     case "-":
                         operand = "sub"
+                        break;
                     case "/":
                         operand = "div"
+                        break;
                     case "*":
                         operand = "mul"
+                        break;
                 };   
-                console.log(operand)
+                firstNum = storedNumber;
+                storedNumber = "";
+                clearDisplay(display);
                 
-            }
+            };
+            if (button === "=") {
+                clearDisplay(display);
+                const result = operate(operand, firstNum, storedNumber)
+                const final = document.createTextNode(result);
+                display.appendChild(final);
+                
+                console.log(firstNum)
+                console.log(storedNumber)
+                storedNumber = parseInt(result);
+
+            };
         };
     });
     
 }
-const add = function(a, b) {return a + b;};
+const add = function(a, b) {
+    a = parseInt(a)
+    b = parseInt(b)
+    return a + b;
+};
 const sub = function(a, b) {return a - b;};
 const mul = function(a, b) {return a * b;};
 const div = function(a, b)  {return a / b;};
@@ -56,5 +78,10 @@ const operate = function(operator, a, b) {
     };
 };
 
+function clearDisplay(display) {
+    while(display.firstChild) {
+        display.removeChild(display.lastChild);
+    }
+}
 
 calculator()
